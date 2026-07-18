b793e15556be4a70ba1e655eae88a755152e1ce9
steppacodes <steppacodes@gmail.com>
2026-07-18 16:19:34 +0100

    fix: enable media file serving in django urls

--- weaponscan/urls.py
+++ weaponscan/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from backend.api import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', api.urls),
 ]
+
+if settings.DEBUG or True:  # Serve media files in dev and production for simple hosting
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
